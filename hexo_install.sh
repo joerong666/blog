cfa534faacae01026aff9a4105d9acd4fd3b355b
joerong666 <joerong666@126.com>
2016-02-17 21:21:01 +0800

    save

--- hexo_install.sh
+++ hexo_install.sh
@@ -3,11 +3,11 @@ u_name="joerong666"
 u_email="joerong666@126.com"
 theme="green-light"
 
-if [ "$u_pass" -eq "init" ]; then
+if [ "$u_pass" = "init" ]; then
     ssh-keygen -t rsa -C "$u_email"
 
     echo "attach the following text to github ssh keys"
-    cat ~/.id_rsa.pub
+    cat ~/.ssh/id_rsa.pub
 elif [ -n "$u_pass" ]; then
 
     mkdir hexo
@@ -18,22 +18,16 @@ elif [ -n "$u_pass" ]; then
 
     cat <<EOF >~/.git-credentials
     https://$u_name:$u_pass@github.com
-    EOF
-
+EOF
     git config --global user.name "$u_name"
     git config --global user.email "$u_email"
     git config --global credential.helper store
-
     git clone https://github.com/$u_name/hexo-theme-${theme}.git themes/$theme
-
     rm -rf source
     git clone https://github.com/$u_name/blog.git source
-
     ln -f source/hexo_config.yml _config.yml
     ln -f source/theme_config.yml themes/${theme}/_config.yml
-
-    sed -i "s/^theme:.*/theme: $them/" _config.yml
-
+    sed -i "s/^theme:.*/theme: $theme/" _config.yml
     npm install
     npm install hexo-deployer-git --save
     npm install hexo-tag-plantuml --save
