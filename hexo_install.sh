1341f3bf6c69a214db35f2ab45ce988a3f11b96d
joerong666 <joerong666@126.com>
2016-02-18 18:21:44 +0800

    save

--- hexo_install.sh
+++ hexo_install.sh
@@ -22,16 +22,22 @@ EOF
     git config --global user.name "$u_name"
     git config --global user.email "$u_email"
     git config --global credential.helper store
+
     git clone https://github.com/$u_name/hexo-theme-${theme}.git themes/$theme
+
     rm -rf source
     git clone https://github.com/$u_name/blog.git source
+
     ln -f source/hexo_config.yml _config.yml
     ln -f source/theme_config.yml themes/${theme}/_config.yml
+
     sed -i "s/^theme:.*/theme: $theme/" _config.yml
+
     npm install
     npm install hexo-deployer-git --save
     npm install hexo-tag-plantuml --save
     npm install hexo-generator-feed --save
+    npm install hexo-renderer-less --save
 else
     echo "$0 [init|<passwd>]"
 fi
